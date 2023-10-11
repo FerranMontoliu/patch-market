9def4bb528ddf64d4c1e8ecf19c8477a1b29f71a
Ferran Montoliu <montoliuferran@gmail.com>
2023-10-11 19:22:29 +0300

    feat: Added routing to the app.

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import './index.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
 
 const rootElement : HTMLElement | null= document.getElementById('root')
 
@@ -15,7 +16,9 @@ if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </QueryClientProvider>
     </StrictMode>,
   )
