1a0597d0b95131d1e362f26384f3bbdb4d472976
Ferran Montoliu <montoliuferran@gmail.com>
2023-10-11 19:21:47 +0300

    feat: Added react-query provider to the app.

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,18 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const rootElement : HTMLElement | null= document.getElementById('root')
 
 if (rootElement) {
+  const queryClient = new QueryClient()
+
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
