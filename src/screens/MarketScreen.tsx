76c496346159382597f8188f5949e60b8f92c4c7
Ferran Montoliu <montoliuferran@gmail.com>
2023-10-12 09:16:02 +0300

    feat: Implemented market screen with search by name.

--- src/screens/MarketScreen.tsx
+++ src/screens/MarketScreen.tsx
@@ -1,10 +1,36 @@
-import { ReactElement } from 'react'
-import { Container, Title } from '@mantine/core'
+import { ReactElement, useEffect, useState } from 'react'
+import { Container, Stack, TextInput, Title } from '@mantine/core'
+import { IconSearch } from '@tabler/icons-react'
+import PatchGrid from '../components/PatchGrid.tsx'
+import { mockOwnPatches, mockPatches } from '../mock-data.ts'
+import { Patch } from '../types.ts'
 
 const MarketScreen = (): ReactElement => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [patches, setPatches] = useState(mockPatches)
+
+  useEffect((): void => {
+    const lowerCaseSearchQuery: string = searchQuery.toLowerCase()
+
+    setPatches(
+      mockOwnPatches
+        .filter((patch: Patch): boolean => patch.title.toLowerCase().includes(lowerCaseSearchQuery)))
+  }, [searchQuery])
+
   return (
     <Container>
-      <Title order={1}>Market</Title>
+      <Stack>
+        <Title order={1}>Trade patches</Title>
+        <TextInput
+          leftSectionPointerEvents="none"
+          leftSection={<IconSearch size={16}/>}
+          placeholder="Search..."
+          radius="md"
+          value={searchQuery}
+          onChange={(event) => setSearchQuery(event.currentTarget.value)}
+        />
+        <PatchGrid patches={patches}/>
+      </Stack>
     </Container>
   )
 }
