5f42dfbf6d49e3b87da51130dc528306c789819e
Ferran Montoliu <montoliuferran@gmail.com>
2023-10-12 07:59:59 +0300

    feat: Created router with all the routes needed for the screens + Added not found page.

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import SignUpScreen from '../screens/SignUpScreen'
+import LoginScreen from '../screens/LoginScreen.tsx'
+import MyPatchesScreen from '../screens/MyPatchesScreen.tsx'
+import PatchDetailsScreen from '../screens/PatchDetailsScreen.tsx'
+import MarketScreen from '../screens/MarketScreen.tsx'
+import NotFoundScreen from '../screens/NotFoundScreen.tsx'
+import MyTradesScreen from '../screens/MyTradesScreen.tsx'
+import TradeDetailsScreen from '../screens/TradeDetailsScreen.tsx'
+import AddPatchScreen from '../screens/AddPatchScreen.tsx'
+import UserProfileScreen from '../screens/UserProfileScreen.tsx'
+
+const AppRouter = (): ReactElement => (
+  <Routes>
+    <Route path="/log-in" element={<LoginScreen />} />
+    <Route path="/sign-up" element={<SignUpScreen />} />
+    <Route path="/my-patches" element={<MyPatchesScreen />} />
+    <Route path="/patch-details/:patchId" element={<PatchDetailsScreen />} />
+    <Route path="/my-trades" element={<MyTradesScreen />} />
+    <Route path="/trade-details/:tradeId" element={<TradeDetailsScreen />} />
+    <Route path="/add-patch" element={<AddPatchScreen />} />
+    <Route path="/user-profile" element={<UserProfileScreen />} />
+    <Route path="/" element={<MarketScreen />} />
+    <Route path="*" element={<NotFoundScreen />} />
+  </Routes>
+)
+
+export default AppRouter
---
+++ src/screens/NotFoundScreen.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from 'react'
+import { Title } from '@mantine/core'
+
+const NotFoundScreen = (): ReactElement => (
+  <Title>404 not found</Title>
+)
+
+export default NotFoundScreen
