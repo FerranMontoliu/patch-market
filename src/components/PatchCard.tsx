5e3c846e1c9b2258f9f79a1c90ab578ec84a019c
Ferran Montoliu <montoliuferran@gmail.com>
2023-10-12 09:15:42 +0300

    feat: Implemented patch card and patch grid.

--- src/components/PatchCard.tsx
+++ src/components/PatchCard.tsx
@@ -1,7 +1,37 @@
 import { ReactElement } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Card, Image, Text } from '@mantine/core'
+import { Patch } from '../types.ts'
 
-const PatchCard = (): ReactElement => {
-  return <></>
+type PatchCardProps = {
+  patch: Patch;
+}
+
+const PatchCard = ({ patch }: PatchCardProps): ReactElement => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src={patch.image}
+          height={160}
+          alt={patch.title}
+          fallbackSrc="https://placehold.co/600x400?font=roboto&text=Placeholder"
+        />
+      </Card.Section>
+
+      <Text fw={500} mt="md" mb="xs" lineClamp={1}>
+        {patch.title}
+      </Text>
+
+      <Text size="sm" c="dimmed" lineClamp={2}>
+        {patch.description}
+      </Text>
+
+      <Button fullWidth mt="md" radius="md" component={RouterLink} to={`/patch-details/${patch.id}`}>
+          See more details
+      </Button>
+    </Card>
+  )
 }
 
 export default PatchCard
--- src/components/PatchGrid.tsx
+++ src/components/PatchGrid.tsx
@@ -1,7 +1,20 @@
 import { ReactElement } from 'react'
+import { Patch } from '../types'
+import { Grid } from '@mantine/core'
+import PatchCard from './PatchCard.tsx'
 
-const PatchGrid = (): ReactElement => {
-  return <></>
+type PatchGridProps = {
+  patches: Array<Patch>;
 }
+
+const PatchGrid = ({ patches }: PatchGridProps): ReactElement => (
+  <Grid>
+    {patches.map((patch) => (
+      <Grid.Col key={patch.id} span={{ base: 12, md: 6, lg: 3 }}>
+        <PatchCard patch={patch} />
+      </Grid.Col>
+    ))}
+  </Grid>
+)
 
 export default PatchGrid
