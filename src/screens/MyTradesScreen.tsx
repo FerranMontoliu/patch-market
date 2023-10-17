c2b04a86f20312998e999a431339c0889d9f7526
nicolac <nicolac@kth.se>
2023-10-17 20:15:16 +0300

    feat: Implemented history.

--- src/screens/MyTradesScreen.tsx
+++ src/screens/MyTradesScreen.tsx
@@ -1,10 +1,29 @@
 import { ReactElement } from 'react'
-import { Container, Title } from '@mantine/core'
-
+import { Container, Title, Grid, Text } from '@mantine/core'
+import { mockPatches } from '../mock-data.ts'
+import HistoryElement from '../components/HistoryListElement.tsx'
 const MyTradesScreen = (): ReactElement => {
   return (
     <Container>
       <Title order={1}>My trades</Title>
+      <Grid align='center' mt="xl" mx="xl">
+        <Grid.Col span={4}>
+          <Text fw={700} lineClamp={1}>
+            Patch
+          </Text>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Text fw={700} lineClamp={1}>
+            Date
+          </Text>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Text fw={700} lineClamp={1}>
+            Status
+          </Text>
+        </Grid.Col>
+      </Grid>
+      {mockPatches.map((patch, index) => (<HistoryElement key={index} patch={patch}></HistoryElement>))}
     </Container>
   )
 }
